795b4956e648edde9b8885fe7a80183b6d0262b3
AlexanderIn3d-tech <Alexander@in3d-tech.com>
2025-11-18 12:49:52 +0200

    update

---
+++ Striker360/Assets/_Scripts/CalibrateHead.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class CalibrateHead : MonoBehaviour
+    {
+
+        private void Start() {
+           Invoke(nameof(Correction), 0.5f);
+        }
+
+        private void Correction()
+        {
+            var camRot = Camera.main.transform.rotation;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, camRot.eulerAngles.y, transform.rotation.eulerAngles.z);
+        }
+    }
+}
